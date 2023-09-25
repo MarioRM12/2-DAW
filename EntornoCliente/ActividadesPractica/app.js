84a76bbd1c0d5b9893909ebbe4432d4e7980dfca
MarioRM12 <117935777+MarioRM12@users.noreply.github.com>
2023-09-25 23:21:49 +0200

    Hasta Ejercicio4

--- EntornoCliente/ActividadesPractica/app.js
+++ EntornoCliente/ActividadesPractica/app.js
@@ -22,15 +22,76 @@ function ej_1() {
 
 function ej_2() {
 
+    /*2.- Mejora el ejercicio anterior. Si los números no son número o son menores o iguales a cero,
+    nos los vuelva a pedir. Podéis utilizar la función isNaN aunque no es necesario.*/
+
+    let num1, num2;
+
+    do {
+        num1 = parseInt(prompt("Escribe el primer número"));
+    } while (isNaN(num1) || num1 <= 0);
+
+    do {
+        num2 = parseInt(prompt("Escribe el segundo número"));
+    } while (isNaN(num2) || num2 <= 0);
+
+    if (num1 === num2) {
+        console.log("Los números son iguales");
+    } else if (num1 > num2) {
+        console.log("El número " + num1 + " es mayor que " + num2);
+    } else {
+        console.log("El número " + num2 + " es mayor que " + num1);
+    }
+
 }
 
 
 function ej_3() {
-    
+
+    /* 3.- Utilizando un bucle, mostrar la suma y la media de los números introducidos hasta
+    introducir un número negativo y ahí mostrar el resultado. */
+
+    let teclado, acumulador = 0, contador = 0
+
+    do {
+        
+        teclado = parseInt(prompt("Escriba un numero >= 0"))
+        contador++
+        acumulador += teclado
+
+    } while (teclado >= 0)
+
+    console.log("La suma es",acumulador,", La media es",acumulador/contador)
+
 }
 
 function ej_4() {
     
+    /* 4.- Hacer un programa que muestre todos los números entre dos números introducidos por el
+    usuario. */
+
+    let num1 = prompt("Escribe el primer número");
+    let num2 = prompt("Escribe el segundo número");
+
+    
+    let iterador;
+
+    if (num1 > num2) {
+        iterador = num2
+        while (iterador <= num1) {
+            console.log(iterador)
+            iterador++
+        }
+    }
+    else
+    {
+        iterador = num1
+        while (iterador <= num2) {
+            console.log(iterador)
+            iterador++
+        }
+    }
+
 }
 
 function ej_5() {
@@ -52,7 +113,7 @@ function ej_8() {
 let opcion;
 
 do {
-    opcion = prompt("Menu...\n\
+    opcion = parseInt(prompt("Menu...\n\
     1. Ejercicio 1\n\
     2. Ejercicio 2\n\
     3. Ejercicio 3\n\
@@ -61,23 +122,32 @@ do {
     6. Ejercicio 6\n\
     7. Ejercicio 7\n\
     8. Ejercicio 8\n\
-    0. Salir")
+    0. Salir"))
     switch (opcion){
-        case 1: ej_1();
+        case 1: 
+            console.log("Hola que tal")
+            ej_1();
         break;
-        case 2: ej_2();
+        case 2: 
+            ej_2();
         break;
-        case 3: ej_3();
+        case 3: 
+            ej_3();
         break;
-        case 4: ej_4();
+        case 4: 
+            ej_4();
         break;
-        case 5: ej_5();
+        case 5:
+            ej_5();
         break;
-        case 6: ej_6();
+        case 6:
+            ej_6();
         break;
-        case 7: ej_7();
+        case 7:
+            ej_7();
         break;
-        case 8: ej_8();
+        case 8:
+            ej_8();
         break;
     }
 } while (opcion > 0);
