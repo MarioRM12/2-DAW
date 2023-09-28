db2634a245fe806a840c5706c521cc887a0b45d8
MarioRM12 <xtyponx@gmail.com>
2023-09-28 12:04:41 +0200

    Avances E.Servidor

---
+++ EntornoServidor/ejercicios_big_decimal-master/src/main/java/org/iesvdm/OnlineShop/Account.java
@@ -0,0 +1,10 @@
+package org.iesvdm.OnlineShop;
+
+import java.lang.foreign.AddressLayout;
+
+public class Account {
+
+    String id;
+    Address address;
+
+}
---
+++ EntornoServidor/ejercicios_big_decimal-master/src/main/java/org/iesvdm/OnlineShop/Product.java
@@ -0,0 +1,4 @@
+package org.iesvdm.OnlineShop;
+
+public class Product {
+}
