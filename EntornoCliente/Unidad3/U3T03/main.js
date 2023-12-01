b3328346c7b0c939be2a2870c6ccb0c54703c168
MarioRM12 <xtyponx@gmail.com>
2023-12-01 14:35:54 +0100

    lista conseguida

--- EntornoCliente/Unidad3/U3T03/main.js
+++ EntornoCliente/Unidad3/U3T03/main.js
@@ -1,56 +1,17 @@
-const baseURL = "http://localhost:3000/peliculas";
+const etqlista = document.getElementById("listado-peliculas");
 
-const ej1=document.getElementById("ejemplo1");
-const ej2=document.getElementById("ejemplo2");
-const ej3=document.getElementById("ejemplo3");
+const BaseURL = "http://localhost:3000";
 
-
-
-//MODELO CALLBACK
-const callbackPeli = (titulo) => {
-    ej1.innerText = titulo;
-}
-
-function leerPeliculaCallback(id, callback ) {
-    let r=null;
-    fetch(baseURL + 'peliculas/' + id).then( (resp) => {
-        resp.json().then( (data) => {
-            callback(data.nombre);
+fetch(BaseURL+"/peliculas")
+    .then(resp => {
+        resp.json().then(lista => {
+            lista.forEach(peli => {
+                etqli = document.createElement("li");
+                etqli.innerText = peli.nombre;
+                etqli.addEventListener('click', () => {
+                    buscarPelicula(peli.id);
+                })
+                etqlista.append(etqli);
+            });
         });
-
-    }).catch( error => console.log("fetch1 error:" + error) );
-    
-}
-
-leerPeliculaCallback(1, callbackPeli);
-
-//////////////////////
-
-//MODELO Promesa .then.catch
-//(async () => {
-//await 
-
-let pelicula={"nombre": "nada"};
-let nombre;
-
-fetch(baseURL + "peliculas/2").then( resp => {
-    if (resp.status == 200) {
-        resp.json().then(data => {
-            pelicula = data;
-            //ej3.innerText = "Película DENTRO: " + data.nombre;
-            console.log("fetch leído");
-        })
-    } else {
-        pelicula=null
-        console.log("La película no existe");
-        //ej3.innerText = "No se localiza la película."
-    }
-}).catch ( error => console.log("fetch2 error:" + error));
-
-//setTimeout(()=>{
-if (pelicula!= null) {
-    nombre = "=>" + pelicula.nombre;
-} else {
-    nombre = "NO EXISTE";
-}
-ej2.innerText = "Película: " + nombre;
+    });
