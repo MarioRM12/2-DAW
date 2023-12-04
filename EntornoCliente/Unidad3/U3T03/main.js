5fe1d4f2a1f44c959f6b8fdce1118f7ee1b89724
MarioRM12 <xtyponx@gmail.com>
2023-12-04 12:49:26 +0100

    tarea casi terminada

--- EntornoCliente/Unidad3/U3T03/main.js
+++ EntornoCliente/Unidad3/U3T03/main.js
@@ -1,17 +1,48 @@
 const etqlista = document.getElementById("listado-peliculas");
-
+const cartelImg = document.getElementById("cartel-img");
+const director = document.getElementById("director");
+const clasificacion = document.getElementById("clasificacion");
 const BaseURL = "http://localhost:3000";
 
-fetch(BaseURL+"/peliculas")
-    .then(resp => {
-        resp.json().then(lista => {
-            lista.forEach(peli => {
-                etqli = document.createElement("li");
-                etqli.innerText = peli.nombre;
-                etqli.addEventListener('click', () => {
-                    buscarPelicula(peli.id);
-                })
-                etqlista.append(etqli);
+// Obtener la lista de películas y crear botones para cada una
+fetch(BaseURL + "/peliculas")
+    .then(resp => resp.json())
+    .then(lista => {
+        const listadoPeliculas = document.getElementById("listado-peliculas");
+        lista.forEach(peli => {
+            const botonPelicula = document.createElement("button");
+            botonPelicula.innerText = peli.nombre;
+            botonPelicula.classList.add("btn", "btn-primary", "mb-2");
+            botonPelicula.addEventListener('click', () => {
+                buscarPelicula(peli.id);
             });
+            listadoPeliculas.querySelector(".btn-group-vertical").appendChild(botonPelicula);
         });
     });
+
+// Función para cargar la información de la película al hacer clic en un botón
+function buscarPelicula(id) {
+    const cartelImg = document.getElementById("cartel-img");
+    cartelImg.src = "assets/gif/giphy.gif"; // Mostrar el gif de carga
+
+    fetch(BaseURL + "/pelicula/" + id)
+        .then(resp => resp.json())
+        .then(pelicula => {
+            director.innerText = "Cargando director...";
+            clasificacion.innerText = "Cargando clasificación...";
+            
+            // Utilizando fetchSlow para cargar lentamente la información del director
+            fetchSlow(BaseURL + "/pelicula/" + id)
+                .then(resp => resp.text())
+                .then(info => {
+                    document.getElementById("director").innerText = info.director;
+                    document.getElementById("clasificacion").innerText = pelicula.clasificacion;
+                    // Cambiar la imagen del cartel al de la película seleccionada
+                    cartelImg.src = `assets/imgs/${pelicula.cartel}`;
+                });
+        });
+}
+
+
+
+
