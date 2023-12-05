3e8711707a920f06c2f0b1a1b25ba27bb3423e06
MarioRM12 <xtyponx@gmail.com>
2023-12-05 10:43:15 +0100

    Tarea terminda

--- EntornoCliente/Unidad3/U3T03/main.js
+++ EntornoCliente/Unidad3/U3T03/main.js
@@ -2,6 +2,7 @@ const etqlista = document.getElementById("listado-peliculas");
 const cartelImg = document.getElementById("cartel-img");
 const director = document.getElementById("director");
 const clasificacion = document.getElementById("clasificacion");
+const estrellas = document.getElementById("estrellas");
 const BaseURL = "http://localhost:3000";
 
 // Obtener la lista de películas y crear botones para cada una
@@ -22,27 +23,37 @@ fetch(BaseURL + "/peliculas")
 
 // Función para cargar la información de la película al hacer clic en un botón
 function buscarPelicula(id) {
-    const cartelImg = document.getElementById("cartel-img");
+    
     cartelImg.src = "assets/gif/giphy.gif"; // Mostrar el gif de carga
+    director.innerText = "Cargando director...";
+    clasificacion.innerText = "Cargando clasificación...";
 
-    fetch(BaseURL + "/pelicula/" + id)
+    // Utilizando fetchSlow para cargar lentamente la información del director
+    fetchSlow(BaseURL + "/pelicula/" + id)
         .then(resp => resp.json())
         .then(pelicula => {
-            director.innerText = "Cargando director...";
-            clasificacion.innerText = "Cargando clasificación...";
-            
-            // Utilizando fetchSlow para cargar lentamente la información del director
-            fetchSlow(BaseURL + "/pelicula/" + id)
-                .then(resp => resp.text())
-                .then(info => {
-                    document.getElementById("director").innerText = info.director;
-                    document.getElementById("clasificacion").innerText = pelicula.clasificacion;
-                    // Cambiar la imagen del cartel al de la película seleccionada
-                    cartelImg.src = `assets/imgs/${pelicula.cartel}`;
-                });
+            director.innerText = pelicula.director;
+            buscarClasificacion(pelicula.clasificacion);
+            estrellas.innerHTML = "";
+            for (let index = 0; index < pelicula.valoracion; index++) {
+                const estrella = document.createElement("i");
+
+                estrella.classList.add("fa");
+                estrella.classList.add("fa-star");
+
+                estrellas.append(estrella);
+               
+            }
+            // Cambiar la imagen del cartel al de la película seleccionada
+            cartelImg.src = `assets/imgs/${pelicula.cartel}`;
         });
+    };
+
+function buscarClasificacion(clasificacionPeli) {
+    fetch(BaseURL + "/clasificaciones/" + clasificacionPeli)
+    .then(resp => {
+        resp.json().then(clasificacionJson => {
+            clasificacion.innerText = clasificacionJson.nombre;
+        })
+    })
 }
-
-
-
-
