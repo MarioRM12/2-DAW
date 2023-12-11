312018716701ce8083b2a538a2d4fca6493f8532
MarioRM12 <xtyponx@gmail.com>
2023-12-11 14:35:43 +0100

    examen suspenso

---
+++ EntornoCliente/Examenes/ExamenDiciembre/ExamenDepuracion/examenDWEC depuración/Proyecto3/main.js
@@ -0,0 +1,8 @@
+//En las casillas deben ponerse con el texto: "Valor asignado"
+let num = 0;
+
+while(num <= 3) {
+    const casilla = document.getElementById("casilla" + num);
+    casilla.innerText = "Valor asignado";
+    num = num +1;
+}
