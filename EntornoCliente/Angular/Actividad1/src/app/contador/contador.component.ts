c2fb39f6f6d5c04fc61f56d10c95a5c5813ac443
MarioRM12 <xtyponx@gmail.com>
2024-01-15 12:51:12 +0100

    15 de enelo

---
+++ EntornoCliente/Angular/Actividad1/src/app/contador/contador.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent {
+  contador: number = 0;
+
+  incrementarContador() {
+    this.contador++;
+  }
+}
